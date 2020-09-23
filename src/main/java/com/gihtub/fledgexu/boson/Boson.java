fd46312413bf7fa7af6d0059eb6416124525b99b
FledgeXu <xzk0701@gmail.com>
2020-09-23 13:22:01 +0800

    Add first block.

--- src/main/java/com/gihtub/fledgexu/boson/Boson.java
+++ src/main/java/com/gihtub/fledgexu/boson/Boson.java
@@ -11,5 +11,7 @@ public class Boson {
         com.gihtub.fledgexu.boson.first_item.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.food.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.melee_weapons.ItemRegistry.ITEMS.register(eventBus);
+        com.gihtub.fledgexu.boson.first_block.ItemRegistry.ITEMS.register(eventBus);
+        com.gihtub.fledgexu.boson.first_block.BlockRegistry.BLOCKS.register(eventBus);
     }
 }
