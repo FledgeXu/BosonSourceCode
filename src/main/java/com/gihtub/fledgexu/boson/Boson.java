29a6eb662d41a6fe7604ae79793fcbe99ba8ee50
FledgeXu <xzk0701@gmail.com>
2020-09-23 13:00:21 +0800

    Add the first item.

--- src/main/java/com/gihtub/fledgexu/boson/Boson.java
+++ src/main/java/com/gihtub/fledgexu/boson/Boson.java
@@ -1,7 +1,13 @@
 package com.gihtub.fledgexu.boson;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Utils.MOD_ID)
 public class Boson {
+    public Boson() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        com.gihtub.fledgexu.boson.first_item.ItemRegistry.ITEMS.register(eventBus);
+    }
 }
