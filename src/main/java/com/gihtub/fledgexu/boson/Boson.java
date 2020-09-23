63dd33132d114a26d33f23ffa9cef004d347444c
FledgeXu <xzk0701@gmail.com>
2020-09-23 12:53:58 +0800

    Mod Init finished.

---
+++ src/main/java/com/gihtub/fledgexu/boson/Boson.java
@@ -0,0 +1,7 @@
+package com.gihtub.fledgexu.boson;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class Boson {
+}
---
+++ src/main/java/com/gihtub/fledgexu/boson/Utils.java
@@ -0,0 +1,5 @@
+package com.gihtub.fledgexu.boson;
+
+public class Utils {
+    public static final String MOD_ID = "boson";
+}
