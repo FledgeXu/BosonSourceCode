227dcb95aa554725992389265caac4952d364a84
FledgeXu <xzk0701@gmail.com>
2020-09-28 20:15:27 +0800

    change send message.

--- src/main/java/com/tutorial/boson/tickabletileentity/ObsidianHelloTileEntity.java
+++ src/main/java/com/tutorial/boson/tickabletileentity/ObsidianHelloTileEntity.java
@@ -20,7 +20,7 @@ public class ObsidianHelloTileEntity extends TileEntity implements ITickableTile
                 PlayerEntity player = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 10, false);
                 TranslationTextComponent translationTextComponent = new TranslationTextComponent("message.hello_block.hello");
                 if (player != null) {
-                    player.sendMessage(translationTextComponent, player.getUniqueID());
+                    player.sendStatusMessage(translationTextComponent, false);
                 }
                 timer = 0;
             }
