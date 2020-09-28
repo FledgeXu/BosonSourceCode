de73f9de11fc71ebc507239b740dbea5369b914b
FledgeXu <xzk0701@gmail.com>
2020-09-28 17:46:37 +0800

    Add tool.

--- src/main/java/com/gihtub/fledgexu/boson/Boson.java
+++ src/main/java/com/gihtub/fledgexu/boson/Boson.java
@@ -15,6 +15,7 @@ public class Boson {
         com.gihtub.fledgexu.boson.first_item.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.food.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.melee_weapons.ItemRegistry.ITEMS.register(eventBus);
+        com.gihtub.fledgexu.boson.tool.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.first_block.ItemRegistry.ITEMS.register(eventBus);
         com.gihtub.fledgexu.boson.first_block.BlockRegistry.BLOCKS.register(eventBus);
         com.gihtub.fledgexu.boson.blockstate.ItemRegistry.ITEMS.register(eventBus);
--- src/main/java/com/gihtub/fledgexu/boson/melee_weapons/ObsidianSword.java
+++ src/main/java/com/gihtub/fledgexu/boson/melee_weapons/ObsidianSword.java
@@ -1,46 +1,11 @@
 package com.gihtub.fledgexu.boson.melee_weapons;
 
-import com.gihtub.fledgexu.boson.first_item.ItemRegistry;
-import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SwordItem;
-import net.minecraft.item.crafting.Ingredient;
 
 public class ObsidianSword extends SwordItem {
-    private static final IItemTier iItemTier = new IItemTier() {
-        @Override
-        public int getMaxUses() {
-            return 2000;
-        }
-
-        @Override
-        public float getEfficiency() {
-            return 10.0F;
-        }
-
-        @Override
-        public float getAttackDamage() {
-            return 4.0F;
-        }
-
-        @Override
-        public int getHarvestLevel() {
-            return 3;
-        }
-
-        @Override
-        public int getEnchantability() {
-            return 30;
-        }
-
-        @Override
-        public Ingredient getRepairMaterial() {
-            return Ingredient.fromItems(ItemRegistry.obsidianIngot.get());
-        }
-    };
-
     public ObsidianSword() {
-        super(iItemTier, 3, -2.4F, new Item.Properties().group(ItemGroup.COMBAT));
+        super(ModItemTier.OBSIDIAN, 3, -2.4F, new Item.Properties().group(ItemGroup.COMBAT));
     }
 }
