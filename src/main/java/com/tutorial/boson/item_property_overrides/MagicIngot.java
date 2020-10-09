31d389f39c9a8a41c234b153449c417d0820aaf3
FledgeXu <xzk0701@gmail.com>
2020-10-09 20:01:00 +0800

    Change group.

--- src/main/java/com/tutorial/boson/item_property_overrides/MagicIngot.java
+++ src/main/java/com/tutorial/boson/item_property_overrides/MagicIngot.java
@@ -1,10 +1,10 @@
 package com.tutorial.boson.item_property_overrides;
 
+import com.tutorial.boson.group.ModGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 
 public class MagicIngot extends Item {
     public MagicIngot() {
-        super(new Properties().group(ItemGroup.MATERIALS));
+        super(new Properties().group(ModGroup.itemGroup));
     }
 }
