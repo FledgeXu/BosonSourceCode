49bbe632b9c7297e3859e4b47cb67a304f21ffeb
FledgeXu <xzk0701@gmail.com>
2020-10-10 13:21:30 +0800

    Add Data generation.

---
+++ src/main/java/com/tutorial/boson/datagen/DataGenEvent.java
@@ -0,0 +1,13 @@
+package com.tutorial.boson.datagen;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenEvent {
+    @SubscribeEvent
+    public static void dataGen(GatherDataEvent event) {
+        event.getGenerator().addProvider(new Recipes(event.getGenerator()));
+    }
+}
