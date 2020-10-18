adf75f3f032fd2874a827877df4d7367cca12cbe
FledgeXu <xzk0701@gmail.com>
2020-10-18 19:30:58 +0800

    Delete dist.

--- src/main/java/com/tutorial/boson/special_render_type/RenderTypeRegistry.java
+++ src/main/java/com/tutorial/boson/special_render_type/RenderTypeRegistry.java
@@ -7,7 +7,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RenderTypeRegistry {
     @SubscribeEvent
     public static void onRenderTypeSetup(FMLClientSetupEvent event) {
